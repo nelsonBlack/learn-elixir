af1a76e7c2df1c84662fca88978be1926837a7ca
Nelson Bwogora <nelsonbwogora@gmail.com>
2022-04-10 12:00:19 +0300

    aluta

--- lib/cards.ex
+++ lib/cards.ex
@@ -7,12 +7,44 @@ defmodule Cards do
   Hello world.
 
   ## Examples
-
+     iex -S mix ----interactive elixir shell
       iex> Cards.hello()
       :world
-
+  multiple methods same name
   """
-  def hello do
-    :world
+  def create_deck do
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
+    suits = [
+      "Hearts",
+      "Diamonds",
+      "Clubs",
+      "Spades"
+    ]
+    for suit <-suits do
+      suit
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
